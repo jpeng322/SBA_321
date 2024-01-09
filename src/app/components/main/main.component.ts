29bb6791d8c59467a0436aedcd24b16deb1b7dfc
Jacky <jackypengsmail@gmail.com>
2024-01-09 00:55:28 -0500

    Add structural directives

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -11,7 +11,11 @@ export class MainComponent {
   fontColor = 'blue';
 
   items = ['Components', 'Templates', 'Directives', "What's next?"];
-
+  cats = [
+    'https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D',
+    'https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.668xw:1.00xh;0.119xw,0&resize=1200:*',
+    'https://images.unsplash.com/photo-1615789591457-74a63395c990?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9tZXN0aWMlMjBjYXR8ZW58MHx8MHx8fDA%3D',
+  ];
   swapDisplay() {
     this.showDefault = !this.showDefault;
   }
@@ -19,7 +23,6 @@ export class MainComponent {
   pointer(hoverName: any) {
     hoverName.style.cursor = 'pointer';
   }
-
 
   showElement: boolean = false;
 }
